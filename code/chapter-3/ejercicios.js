6df8d50f54377a6d3a2f0f9b0f80d6ec490f2f68
Jose Luis <wildchamo@gmail.com>
2025-10-02 20:20:55 -0500

    feat: add recursive isOdd function and string character counting utilities

--- code/chapter-3/ejercicios.js
+++ code/chapter-3/ejercicios.js
@@ -7,17 +7,48 @@
     }
   }
 }
+
 {
-  function isOdd(n, mem) {
-    let memory;
+  function isOdd(n) {
     if (n === 0) {
       return false;
-    } else if (n === 1) {
+    } else if (n === 1 || n === -1) {
       return true;
     } else {
-      memory = n - 2;
+      return isOdd(n - 2);
     }
   }
 
-  isOdd(12, 0);
+  const isOdd2 = isOdd(13);
+  console.log(isOdd2);
+}
+
+{
+  // count how many B are in the string
+  function countBs(string) {
+    let numberofBs = 0;
+
+    for (i = 0; i <= string.length; i++) {
+      if (string[i] === "B") {
+        numberofBs++;
+      }
+    }
+
+    return numberofBs;
+  }
+
+  console.log(countBs("ABCBBBB"));
+
+  function countCharacters(string, letterToBeSearch) {
+    let numberofCharacters = 0;
+
+    for (i = 0; i <= string.length; i++) {
+      if (string[i] === letterToBeSearch) {
+        numberofCharacters++;
+      }
+    }
+
+    return numberofCharacters;
+  }
+  console.log(countCharacters("ABCBBBB", "A"));
 }
