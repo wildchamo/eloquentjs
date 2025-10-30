91e36af282c884caa761571d9ad8986d010c9737
Jose Luis Bedoya <wildchamo@gmail.com>
2025-10-30 13:14:11 -0500

    feat: add class implementations and prototype examples
    - Added Rabbit class with constructor and speak method to demonstrate object-oriented patterns
    - Implemented SecretiveObject class showcasing private methods using # prefix
    - Created RandomSource class with private field for generating random numbers
    - Added makeRabbit factory function as an alternative object creation approach
    - Included examples of prototype chain manipulation and property inheritance
    - Added demonstration of class expressions

--- code/chapter-6/index.js
+++ code/chapter-6/index.js
@@ -42,3 +42,96 @@ let protoRabbit = {
 let blackRabbit = Object.create(protoRabbit);
 // blackRabbit.type = "black";
 blackRabbit.speak("Im fear and darkness");
+
+function makeRabbit(type) {
+  let rabbit = Object.create(protoRabbit);
+  rabbit.type = type;
+  return rabbit;
+}
+
+class Rabbit {
+  constructor(type) {
+    this.type = type;
+  }
+
+  speak(line) {
+    console.log("the " + this.type + " rabbit says " + line);
+  }
+}
+
+const newRabbit = new Rabbit("new");
+const killerRabbit = new Rabbit("killer");
+
+newRabbit.speak("hola");
+killerRabbit.speak("hola Jose");
+
+function proto() {}
+
+console.log(Object.getPrototypeOf(proto));
+console.log(Object.getPrototypeOf(proto) === Function.prototype);
+console.log(Object.getPrototypeOf(killerRabbit) === Rabbit.prototype);
+
+class particle {
+  speed = 0;
+  constructor(position) {
+    this.position = position;
+  }
+}
+
+let object = new (class {
+  getWord() {
+    return "hello";
+  }
+})();
+
+console.log(object.getWord());
+
+class SecretiveObject {
+  #getSecret() {
+    return "I ate all the plums";
+  }
+
+  interrogate() {
+    let shallISayIt = this.#getSecret();
+    return "never";
+  }
+}
+
+const secretive = new SecretiveObject();
+console.log(secretive.interrogate());
+// console.log(secretive.#getSecret());
+
+class RandomSource {
+  #max;
+
+  constructor(max) {
+    this.#max = max;
+  }
+
+  getNumber() {
+    return Math.floor(Math.random() * this.#max);
+  }
+}
+
+const randomSource = new RandomSource(55);
+
+console.log(randomSource.getNumber());
+
+{
+  class Rabbit {
+    constructor(type) {
+      this.type = type;
+    }
+
+    speak(line) {
+      console.log("the " + this.type + " rabbit says " + line);
+    }
+  }
+
+  const newRabbit = new Rabbit("new");
+  const killerRabbit = new Rabbit("killer");
+  Rabbit.prototype.teeth = "small";
+  console.log(killerRabbit.teeth);
+  killerRabbit.teeth = "long, sharp";
+  console.log(killerRabbit.teeth);
+}
