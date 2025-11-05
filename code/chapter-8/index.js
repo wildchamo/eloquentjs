af5e86b96909b8eac9af0312b819ae424c829de8
Jose Luis Bedoya <wildchamo@gmail.com>
2025-11-05 16:11:18 -0500

    feat: add chapter 8 code examples for JavaScript concepts
    - Demonstrates strict mode usage and basic loop iteration
    - Shows constructor function pattern for object creation
    - Includes placeholder for graph route-finding function

---
+++ code/chapter-8/index.js
@@ -0,0 +1,22 @@
+"use strict";
+function canYouSoptTheProblem() {
+  "use strict";
+  for (let counter = 0; counter < 10; counter++) {
+    console.log("Happy happy");
+  }
+}
+canYouSoptTheProblem();
+{
+  function Person(name) {
+    this.name = name;
+  }
+
+  let ferdinand = new Person("Ferdinand");
+
+  console.log(ferdinand);
+}
+
+{
+  // (graph: Object) ...
+  function findROute(graph, from, to) {}
+}
