7e3350bf0a7ca6b1ef0b3891102a84f8d4f92b5b
Jose Luis Bedoya <wildchamo@gmail.com>
2025-11-08 20:59:47 -0500

    feat: add negated character class example to regex demonstrations

--- code/chapter-9/index.js
+++ code/chapter-9/index.js
@@ -11,6 +11,7 @@ console.log(aPlus.test("A+"));
 
 console.log(/[0123456789]/.test(" in 1992"));
 console.log(/[0-9]/.test(" in 1992"));
+console.log(/[^0-9]/.test("in 1992"));
 
 // \d any digit
 // \w aplanumeric
